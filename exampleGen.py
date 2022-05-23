2f4a5e6075cf8db5178bc1bab2a48f6349d8ec93
teshim <104706335+teshim@users.noreply.github.com>
2022-05-23 05:08:18 -0700

    Add files via upload
    Added bare-bones training example generation. TODO: Export data as CSV.

---
+++ exampleGen.py
@@ -0,0 +1,82 @@
+import sys
+import random
+
+CARDS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+VALS = {'a': 1, 'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
+
+
+def generateOneHand():
+    deck = CARDS * 4
+    random.shuffle(deck)
+    dealerHand = [deck[0], deck[1]]
+    playerHand = [deck[2], deck[3]]
+    shouldHit = None
+    playerVal = sum([VALS[card] for card in playerHand])
+    originalPlayerVal = playerVal
+    print(playerHand, dealerHand)
+    tryHitting = random.randint(0, 1)
+
+    # Try hitting
+    if tryHitting == 1 and shouldHit is None:
+        print("Try hitting")
+        playerHand.append(deck[4])
+        playerVal += VALS[deck[4]]
+        if playerVal > 21:  # We busted
+            if 'A' in playerHand:
+                playerVal -= 10
+                playerAce = playerHand.index('A')
+                playerHand[playerAce] = 'a'
+            else:
+                shouldHit = False
+        if shouldHit is None:
+            dealerVal = sum([VALS[card] for card in dealerHand])
+            i = 5
+            while dealerVal < 17:
+                dealerHand.append(deck[i])
+                dealerVal += VALS[deck[i]]
+                if dealerVal > 21:
+                    if 'A' in dealerHand:
+                        aceIndex = dealerHand.index('A')
+                        dealerHand[aceIndex] = 'a'
+                        dealerVal -= 10
+                    else:
+                        shouldHit = True
+                        break
+                i += 1
+        if shouldHit is None:
+            shouldHit = playerVal >= dealerVal or (playerVal < dealerVal and playerVal >= originalPlayerVal)
+        print(playerHand, dealerHand)
+        if shouldHit:
+            print("Hitting was correct")
+        else:
+            print("Hitting was incorrect")
+    # Try staying
+    elif tryHitting == 0 and shouldHit is None:
+        print("Try staying")
+        dealerVal = sum([VALS[card] for card in dealerHand])
+        i = 4
+        while dealerVal < 17:
+            dealerHand.append(deck[i])
+            dealerVal += VALS[deck[i]]
+            if dealerVal > 21:
+                if 'A' in dealerHand:
+                    aceIndex = dealerHand.index('A')
+                    dealerHand[aceIndex] = 'a'
+                    dealerVal -= 10
+                else:
+                    shouldHit = False
+                    break
+            i += 1
+        if shouldHit is None:
+            shouldHit = playerVal < dealerVal
+        print(playerHand, dealerHand)
+        if not shouldHit:
+            print("Staying was correct")
+        else:
+            print("Staying was incorrect")
+
+def main():
+    generateOneHand()
+
+if __name__ == '__main__':
+    main()
