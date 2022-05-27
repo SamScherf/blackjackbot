ebc02287feeab03d6172d118acf98384f153b362
Sam <github@samscherf.com>
2022-05-26 19:59:42 -0700

    Added more testing metrics

--- octave/test_model.m
+++ octave/test_model.m
@@ -12,9 +12,20 @@ _size = get_size();
 [X y] = get_XY("../data/crossVal.txt");
 % [X y] = get_XY("../data/test.txt");
 
-% Check accuracy
+% Get prediction
 pred = predict(Theta1, Theta2, X);
-accuracy =  mean(double(pred == y))
+
+% Get confusion matrix
+tp = sum((pred == 1) & (y == 1));
+fp = sum((pred == 1) & (y == 0));
+fn = sum((pred == 0) & (y == 1));
+tn = sum((pred == 0) & (y == 0));
+
+% Get accuracy, precision, recall, and F1 score
+accuracy = (tp+tn)/(tp+tn+fp+fn)
+prec = tp/(tp+fp)
+rec = tp/(tp+fn)
+F1 = 2*prec*rec/(prec+rec)
 
 % Save results
 dlmwrite("result.csv", [X y pred], ",");
