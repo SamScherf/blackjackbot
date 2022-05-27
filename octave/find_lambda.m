dc363835ae9a3dcec0e7f0a2d425ca19c258f1b4
Sam <github@samscherf.com>
2022-05-26 22:22:08 -0700

    Added script to find best lambda

---
+++ octave/find_lambda.m
@@ -0,0 +1,24 @@
+% Add path
+addpath('utils/');
+
+% Get training and cross validation data
+[X y] = get_XY("../data/trainingv2.txt");
+[X_cv y_cv] = get_XY("../data/crossVal.txt");
+
+% Try various lambdas error
+[lambda_vec, error_train, error_cv] = validationCurve(X, y, X_cv, y_cv);
+
+% Find best lambda
+[val, idx] = min(error_cv);
+best_lambda = lambda_vec(idx)
+
+% Plot results
+hf = figure();
+plot(lambda_vec, error_train);
+hold on;
+plot(lambda_vec, error_cv);
+xlabel("Lambda");
+ylabel("Error");
+title("Validation Curve");
+legend('Training Error', 'Cross Validation Error')
+print(hf, "plot.png");
---
+++ octave/utils/validationCurve.m
@@ -0,0 +1,32 @@
+function [lambda_vec, error_train, error_cv] = validationCurve(X, y, X_cv, y_cv)
+
+% Values of lambda to try
+lambda_vec = [0 0.001 0.003 0.001 0.003 0.01 0.03 0.1 0.3 1]';
+
+% Initialize accuracy vectors
+error_train = zeros(length(lambda_vec), 1);
+error_cv = zeros(length(lambda_vec), 1);
+
+% Get size of model
+_size = get_size();
+
+% Loop through every lambda
+for i=1:length(lambda_vec)
+
+	% Randomly initialize parameters
+	nn_params = init_params(_size);
+
+	% Train model with given lambda
+	options = optimset('MaxIter', 400);
+	costFunction = @(p) nnCostFunction(p, _size, X, y, lambda_vec(i));
+	[nn_params, cost] = fmincg(costFunction, nn_params, options);
+
+    	% Get predictions
+	J_train = nnCostFunction(nn_params, _size, X, y, 0);
+	J_cv = nnCostFunction(nn_params, _size, X_cv, y_cv, 0);
+
+    	% Add values to matrices
+    	error_train(i) = J_train;
+    	error_cv(i) = J_cv;
+end
+end
